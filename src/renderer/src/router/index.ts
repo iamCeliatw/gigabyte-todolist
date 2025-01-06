cc06c931b19901cf82e9bdda866eebec1d4fcfda
MacMini <celia.chang@kingone-design.com>
2025-01-06 13:02:58 +0800

    init

--- src/renderer/src/router/index.ts
+++ src/renderer/src/router/index.ts
@@ -5,16 +5,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: () => import('@renderer/views/Home.vue')
-  },
-  {
-    path: '/ai',
-    name: 'AI',
-    component: () => import('@renderer/views/AI.vue')
-  },
-  {
-    path: '/Show',
-    name: 'Show',
-    component: () => import('@renderer/views/Show.vue')
   }
 ]
 
